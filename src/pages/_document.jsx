39804cb43d3fc68d3cc3d4f30dc5a3a5cb3414ca
Students Tools <noreply@studentstools.site>
2026-04-05 16:52:21 +0800

    Add: Google Search Console verification meta tag

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="_DX-IqQOzxxhF1LgUkQ6Yko-kH2q5SecqbvOtl9fElA" />
+
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-RLJ40YDPPQ"></script>
         <script
